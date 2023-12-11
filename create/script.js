e40fb01c0d8aed0395cd26942a677c6cd5e0b5e5
Alex6s <alexis.delfosse01@gmail.com>
2023-12-11 16:31:45 +0100

    Creer

--- create/script.js
+++ create/script.js
@@ -1,2 +1,33 @@
-/*
-*/
+// Function to generate a random hexadecimal color
+function getRandomColor() {
+return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Learners and their corresponding random background colors
+const learners = [
+{ name: 'Julien', color: getRandomColor() },
+{ name: 'Jeremy', color: getRandomColor() },
+{ name: 'Alice', color: getRandomColor() }
+];
+
+// Reference to the article element
+const article = document.querySelector('article');
+
+// Loop through the learners array
+for (let i = 0; i < learners.length; i++) {
+// Create a section element
+const section = document.createElement('section');
+
+// Set background color
+section.style.backgroundColor = learners[i].color;
+
+// Create a paragraph for the learner's name
+const paragraph = document.createElement('p');
+paragraph.textContent = learners[i].name;
+
+// Append paragraph to section
+section.appendChild(paragraph);
+
+// Append section to the article
+article.appendChild(section);
+}
