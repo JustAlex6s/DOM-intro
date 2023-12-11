7cec59b919c822a4b0644d8ab0e3eebd81a14c26
Alex6s <alexis.delfosse01@gmail.com>
2023-12-11 16:40:15 +0100

    scirpt js changement couleur creer fait

--- create/script.js
+++ create/script.js
@@ -1,33 +1,17 @@
-// Function to generate a random hexadecimal color
-function getRandomColor() {
-return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-// Learners and their corresponding random background colors
-const learners = [
-{ name: 'Julien', color: getRandomColor() },
-{ name: 'Jeremy', color: getRandomColor() },
-{ name: 'Alice', color: getRandomColor() }
-];
-
-// Reference to the article element
-const article = document.querySelector('article');
-
-// Loop through the learners array
-for (let i = 0; i < learners.length; i++) {
-// Create a section element
-const section = document.createElement('section');
-
-// Set background color
-section.style.backgroundColor = learners[i].color;
-
-// Create a paragraph for the learner's name
-const paragraph = document.createElement('p');
-paragraph.textContent = learners[i].name;
-
-// Append paragraph to section
-section.appendChild(paragraph);
-
-// Append section to the article
-article.appendChild(section);
-}
+function creationDiv(nom){
+    let article = document.querySelectorAll('article');
+    for (let elem of article){
+    let newsection = document.createElement('section');
+    let newcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    let newparagraphe = document.createElement('p');
+    newparagraphe.innerHTML = nom;
+    newsection.appendChild(newparagraphe);
+    newsection.style.backgroundColor = newcolor;
+    elem.appendChild(newsection);
+    
+    }
+    }
+    let tab = ["alice", "julien", "alexis","cassidy", "winona","hanen"];
+    for (let i = 0; i< tab.length; i++){
+    creationDiv(tab[i]);
+    }
