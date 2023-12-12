9c6b0f87e8c5c4542f1f90feb85e2166ad6ae01e
Alex6s <alexis.delfosse01@gmail.com>
2023-12-12 09:57:44 +0100

    exo create faits

--- create/script.js
+++ create/script.js
@@ -1,17 +1,66 @@
-function creationDiv(nom){
-    let article = document.querySelectorAll('article');
-    for (let elem of article){
-    let newsection = document.createElement('section');
-    let newcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    let newparagraphe = document.createElement('p');
-    newparagraphe.innerHTML = nom;
-    newsection.appendChild(newparagraphe);
-    newsection.style.backgroundColor = newcolor;
-    elem.appendChild(newsection);
-    
+
+//Modifiez le script.js pour créer une nouvelle <section> avec une couleur de fond aléatoire pour chaque apprenant de votre groupe. Cette section doit contenir un paragraphe avec le nom de l'apprenant. Ces sections doivent être ajoutées dans la section <article>.
+
+function creationDiv(nom) {
+    let articles = document.querySelectorAll('article');
+
+    for (let article of articles) {
+        let newSection = document.createElement('section');
+        let newColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        let newParagraph = document.createElement('p');
+        newParagraph.innerHTML = nom;
+        newSection.appendChild(newParagraph);
+        newSection.style.backgroundColor = newColor;
+
+        // Ajouter la classe pour identifier le texte à ajuster
+        newParagraph.classList.add('adjust-text-color');
+
+        article.appendChild(newSection);
+
+        // Appeler la fonction pour ajuster la couleur du texte
+        ajusterCouleurTexte();
     }
+}
+// Changer le couleur de mon texte en fonction de la couleur background
+
+function ajusterCouleurTexte() {
+    let textElements = document.querySelectorAll('.adjust-text-color');
+
+    for (let textElement of textElements) {
+        // Obtenir la couleur de fond calculée
+        let backgroundColor = getComputedStyle(textElement.parentElement).backgroundColor;
+        let rgb = backgroundColor.match(/\d+/g);
+
+        if (rgb) {
+            let brightness = calculateBrightness(rgb);
+
+            // Définir la couleur du texte en fonction du contraste
+            textElement.style.color = brightness > 128 ? 'black' : 'white';
+        } else {
+            console.error('Impossible de lire la couleur de fond.');
+        }
     }
-    let tab = ["alice", "julien", "alexis","cassidy", "winona","hanen"];
-    for (let i = 0; i< tab.length; i++){
+}
+
+function calculateBrightness(rgb) {
+    // Assurez-vous que les valeurs RVB sont des nombres
+    let r = parseInt(rgb[0]);
+    let g = parseInt(rgb[1]);
+    let b = parseInt(rgb[2]);
+
+    return (r * 299 + g * 587 + b * 114) / 1000;
+}
+
+let tab = ["alice", "julien", "alexis", "cassidy", "winona", "hanen"];
+shuffleArray(tab);
+for (let i = 0; i < tab.length; i++) {
     creationDiv(tab[i]);
+}
+
+//Trouvez un moyen pour que chaque fois que vous chargez la page, l'ordre des éléments change !
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
+}
